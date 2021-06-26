09dc50e2f8d54b599684cde2b8182bacdd57ad50
anonymousliem <muslimsyuhadah@gmail.com>
2021-06-26 22:41:01 +0700

    transhift from id to en

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/anonymousliem/transhift
+
+go 1.16
+
+require (
+	github.com/bregydoc/gtranslate v0.0.0-20200913051839-1bd07f6c1fc5
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/robertkrimen/otto v0.0.0-20210614181706-373ff5438452 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -2,23 +2,38 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/bregydoc/gtranslate"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/julienschmidt/httprouter"
+	"net/http"
+	_ "net/url"
 )
 
 func main() {
-	text := "Hello World"
-	translated, err := gtranslate.TranslateWithParams(
-		text,
-		gtranslate.TranslationParams{
-			From: "en",
-			To:   "ja",
-		},
-	)
-	if err != nil {
-		panic(err)
+	router := httprouter.New()
+	var url = "/transhift"
+	router.GET(url, func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		queryValues := request.URL.Query()
+		text := queryValues.Get("q")
+		translated, err := gtranslate.TranslateWithParams(
+			text,
+			gtranslate.TranslationParams{
+				From: "id",
+				To:   "en",
+			},
+		)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(writer, translated)
+
+	})
+	server := http.Server{
+		Handler: router,
+		Addr: "localhost:3000",
 	}
 
-	fmt.Printf("en: %s | ja: %s \n", text, translated)
-	// en: Hello World | ja: こんにちは世界
+	server.ListenAndServe()
+
 }
